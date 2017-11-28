a19116599e46e2c3d6d63e9f47ff2b8b56005b51
Momoko Ishimura <applemelody03@gmail.com>
2017-11-29 08:22:31 +0900

    first commit.

---
+++ app/src/main/java/com/example/piano2/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.piano2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.media.MediaPlayer
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    var mpDo: MediaPlayer?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // View要素を変数に代入
+        val doButton: Button = findViewById(R.id.doButton)
+
+        mpDo = MediaPlayer.create(applicationContext, R.raw.pianodo)
+
+        // ド
+        doButton.setOnClickListener {
+            mpDo?.start()
+        }
+    }
+}
