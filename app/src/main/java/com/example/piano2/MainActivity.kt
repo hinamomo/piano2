c8befed142113dd23e85d0b3621f019c29e85692
Momoko Ishimura <applemelody03@gmail.com>
2017-11-29 08:43:52 +0900

    add mpfile.

--- app/src/main/java/com/example/piano2/MainActivity.kt
+++ app/src/main/java/com/example/piano2/MainActivity.kt
@@ -8,6 +8,13 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
 
     var mpDo: MediaPlayer?= null
+    var mpRe: MediaPlayer?= null
+    var mpMi: MediaPlayer?= null
+    var mpFa: MediaPlayer?= null
+    var mpSo: MediaPlayer?= null
+    var mpRa: MediaPlayer?= null
+    var mpSi: MediaPlayer?= null
+    var mpDoh: MediaPlayer?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,12 +22,54 @@ class MainActivity : AppCompatActivity() {
 
         // View要素を変数に代入する
         val doButton: Button = findViewById(R.id.doButton)
+        val reButton: Button = findViewById(R.id.reButton)
+        val miButton: Button = findViewById(R.id.miButton)
+        val faButton: Button = findViewById(R.id.faButton)
+        val soButton: Button = findViewById(R.id.soButton)
+        val raButton: Button = findViewById(R.id.raButton)
+        val siButton: Button = findViewById(R.id.siButton)
+        val dohButton: Button = findViewById(R.id.dohButton)
 
-        mpDo = MediaPlayer.create(applicationContext, R.raw.pianodo)
+        mpDo = MediaPlayer.create(applicationContext, R.raw.piano_do)
+        mpRe = MediaPlayer.create(applicationContext, R.raw.piano_re)
+        mpMi = MediaPlayer.create(applicationContext, R.raw.piano_mi)
+        mpFa = MediaPlayer.create(applicationContext, R.raw.piano_fa)
+        mpSo = MediaPlayer.create(applicationContext, R.raw.piano_so)
+        mpRa = MediaPlayer.create(applicationContext, R.raw.piano_ra)
+        mpSi = MediaPlayer.create(applicationContext, R.raw.piano_si)
+        mpDoh = MediaPlayer.create(applicationContext, R.raw.piano_doh)
 
-        // ド
         doButton.setOnClickListener {
             mpDo?.start()
         }
+
+        reButton.setOnClickListener {
+            mpRe?.start()
+        }
+
+        miButton.setOnClickListener {
+            mpMi?.start()
+        }
+
+        faButton.setOnClickListener {
+            mpFa?.start()
+        }
+
+        soButton.setOnClickListener {
+            mpSo?.start()
+        }
+
+        raButton.setOnClickListener {
+            mpRa?.start()
+        }
+
+        siButton.setOnClickListener {
+            mpSi?.start()
+        }
+
+        dohButton.setOnClickListener {
+            mpDoh?.start()
+        }
+
     }
 }
